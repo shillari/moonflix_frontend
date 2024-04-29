9f47f9a73a66f9774300b038c7e4f4346f5fcbdd
shillari <sandy_hmh2@hotmail.com>
2024-04-29 09:12:27 +0200

    fetching data from API hosted on heroku

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,10 +5,22 @@ export const MovieView = ({movie, onBackClick}) => {
                 <img src={movie.imagePath}/>
             </div>
             <div>
+                <span>Title: {movie.title}</span>
+            </div>
+            <div>
                 <span>Description: {movie.description}</span>
             </div>
             <div>
+                <span>Year: {movie.year}</span>
+            </div>
+            <div>
+                <span>Genre: {movie.genre.name}</span>
+            </div>
+            <div>
                 <span>Director: {movie.director.name}</span>
+            </div>
+            <div>
+                <span>Actors: {movie.actors[0].name} | {movie.actors[1].name} | {movie.actors[2].name}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
